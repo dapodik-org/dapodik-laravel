35b477a33e806285c4f28bbad071e81977a77a98
Ade Reksi Susanto <adereksisusanto@gmail.com>
2026-07-14 00:34:49 +0700

    refactor GitHub Actions workflows and enhance PHPUnit configuration

--- tests/API/TestCase.php
+++ tests/API/TestCase.php
@@ -18,4 +18,27 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
     }
+
+    protected function resolveApplicationBootstrappers($app)
+    {
+        $app->make('Illuminate\Foundation\Bootstrap\HandleExceptions')->bootstrap($app);
+
+        error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
+        $app->make('Illuminate\Foundation\Bootstrap\RegisterFacades')->bootstrap($app);
+        $app->make('Illuminate\Foundation\Bootstrap\SetRequestForConsole')->bootstrap($app);
+        $app->make('Illuminate\Foundation\Bootstrap\RegisterProviders')->bootstrap($app);
+
+        if (\method_exists($this, 'parseTestMethodAnnotations')) {
+            $this->parseTestMethodAnnotations($app, 'environment-setup');
+            $this->parseTestMethodAnnotations($app, 'define-env');
+        }
+
+        $this->defineEnvironment($app);
+        $this->getEnvironmentSetUp($app);
+
+        $app->make('Illuminate\Foundation\Bootstrap\BootProviders')->bootstrap($app);
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+    }
 }
