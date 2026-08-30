7b792b4200760671af717ffa2c9fc31e55d73032
Ade Reksi Susanto <adereksisusanto@gmail.com>
2026-08-30 21:57:00 +0700

    chore: move MigrateCommandTest to separate test suite

--- tests/Eloquent/MigrateCommand/MigrateCommandTest.php
+++ tests/Eloquent/MigrateCommand/MigrateCommandTest.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
 
-/**
- * @group migrate-command
- */
 class MigrateCommandTest extends TestCase
 {
     private $publishedMigrationsPath;
